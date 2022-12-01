2622821e770466ea40f0c2d9eb69496cd650bc2b
Eshaan Aggarwal <eshaanagg@gmail.com>
2022-12-01 15:52:05 +0530

    Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,8 @@
 use std::cmp::max;
 
-pub fn part1(){
-    
-    let mut max_candies:u32 = 0;
-    
+pub fn part1() {
+    let mut max_candies: u32 = 0;
+
     for elf in include_str!("inputs/1.txt").split("\n\n") {
         let candies = elf.split("\n").map(|x| x.parse::<u32>().unwrap()).sum();
         max_candies = max(max_candies, candies);
@@ -12,21 +11,19 @@ pub fn part1(){
     println!("{max_candies}");
 }
 
-pub fn part2(){
-    
+pub fn part2() {
     let mut candies: Vec<u32> = Vec::new();
-    
+
     for elf in include_str!("inputs/1.txt").split("\n\n") {
         let c = elf.split("\n").map(|x| x.parse::<u32>().unwrap()).sum();
         candies.push(c);
     }
     candies.sort();
 
-    let mut max_candies:u32 = 0;
-    for i in candies.len()-3..candies.len() {
+    let mut max_candies: u32 = 0;
+    for i in candies.len() - 3..candies.len() {
         max_candies += candies[i];
     }
 
     println!("{max_candies}");
 }
-
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn bench(f: fn()) {
     let ret = f();
     println!("Time used: {:?}", time::Instant::now().duration_since(t0));
     println!();
-    
+
     ret
 }
 
