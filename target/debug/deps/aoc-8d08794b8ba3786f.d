fbfc7d59973d6b5f6fd7f8878e68ce5422c646b1
Eshaan Aggarwal <eshaanagg@gmail.com>
2022-12-01 12:18:00 +0530

    Remove unnecessary files

--- target/debug/deps/aoc-8d08794b8ba3786f.d
+++
@@ -1,7 +0,0 @@
-/home/eshaan/Desktop/AdventOfCode22/target/debug/deps/aoc-8d08794b8ba3786f: src/main.rs src/day1.rs src/inputs/1.txt
-
-/home/eshaan/Desktop/AdventOfCode22/target/debug/deps/aoc-8d08794b8ba3786f.d: src/main.rs src/day1.rs src/inputs/1.txt
-
-src/main.rs:
-src/day1.rs:
-src/inputs/1.txt:
