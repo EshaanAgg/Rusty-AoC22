20b27e10ce5e95abe7edf6bebf20ad4dbe9e8edb
Eshaan Aggarwal <eshaanagg@gmail.com>
2022-12-01 12:20:18 +0530

    Typo fix

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn bench(f: fn()) {
     let t0 = time::Instant::now();
     let ret = f();
     println!("Time used: {:?}", time::Instant::now().duration_since(t0));
-    println!()
+    println!();
     
     ret
 }
